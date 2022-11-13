4e0591e37661a91f062b6e7170c6ed26933332b2
Rishabh Singh <rishabhjuly11@gmail.com>
2022-11-14 01:56:09 +0530

    made some changes in Cards

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ function App() {
     <>
       <Navbar />
       <div className="d-flex gap-4 justify-content-center pt-5">
-        <Cards title="JavaScript" 
-        src="https://images.pexels.com/photos/5483077/pexels-photo-5483077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        prag = "lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10"
+        <Cards title="Reymond" 
+        src="https://rukminim1.flixcart.com/image/832/832/xif0q/suit/5/w/1/36-suit-maroon-darbar-in-original-imag5fjyvg3zzaa8-bb.jpeg?q=70"
+        href="https://www.flipkart.com/darbar-slim-fit-solid-men-suit/p/itm7f7c3a19ea3f2?pid=SUIG5FJFTYQHGYPV&lid=LSTSUIG5FJFTYQHGYPVUEJCNV&marketplace=FLIPKART&store=clo%2Fupk%2Fsxw%2Fdoc&srno=b_1_1&otracker=hp_reco_Winter%2BEssentials_1_7.dealCard.OMU_cid%3AS_F_N_clo_upk_sxw_doc__d_50-100__NONE_ALL%3Bnid%3Aclo_upk_sxw_doc_%3Bet%3AS%3Beid%3Aclo_upk_sxw_doc_%3Bmp%3AF%3Bct%3Ad%3B_6&otracker1=hp_reco_WHITELISTED_personalisedRecommendation%2FC5_Winter%2BEssentials_DESKTOP_HORIZONTAL_dealCard_cc_1_NA_view-all_6&fm=Search&iid=en_7tWmxvW%2FxhGteVJcqJ%2BxHDpdaok6489ct943608KAE815v2cPR6H3fZ25%2F8Ic7u7o2KyVo%2F06w5A85DJa%2FvQdg%3D%3D&ppt=browse&ppn=browse"
+        prag = "SLIM FIT Solid Men Suit"
         />
-        <Cards title="ReactJS" 
-        src="https://images.pexels.com/photos/5483077/pexels-photo-5483077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        prag = "lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10"
+        <Cards title="Peter England" 
+        src="https://rukminim1.flixcart.com/image/832/832/xif0q/suit/j/c/v/44-navy-darbar-in-original-imag5gtzaxgqhrqs-bb.jpeg?q=70"
+        href="https://www.flipkart.com/darbar-formal-solid-men-suit/p/itm45b603f69cd83?pid=SUIG5GTZMVSB8STT&lid=LSTSUIG5GTZMVSB8STTHIRDPY&marketplace=FLIPKART&fm=factBasedRecommendation%2FrecentlyViewed&iid=R%3Arv%3Bpt%3App%3Buid%3Ae9b2da57-638b-11ed-bbb1-494f9de85ced%3B.SUIG5GTZMVSB8STT&ppt=pp&ppn=pp&ssid=iqhppkcuzk0000001668368783721&otracker=pp_reco_Recently%2BViewed_1_38.productCard.RECENTLY_VIEWED_Darbar%2BIn%2BFormal%2BSolid%2BMen%2BSuit_SUIG5GTZMVSB8STT_factBasedRecommendation%2FrecentlyViewed_0&otracker1=pp_reco_PINNED_factBasedRecommendation%2FrecentlyViewed_Recently%2BViewed_DESKTOP_HORIZONTAL_productCard_cc_1_NA_view-all&cid=SUIG5GTZMVSB8STT"
+        prag = "Formal Solid Men Suit"
         />
-        <Cards title="NodeJS and Express" 
-        src="https://images.pexels.com/photos/5483077/pexels-photo-5483077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        prag = "lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10lorem10"
+        <Cards title="Manyawar" 
+        src="https://rukminim1.flixcart.com/image/832/832/kb1470w0/ethnic-set/h/y/a/44-kpw-abh-metty-abh-lifestyle-original-imafsh83ump4vhs9.jpeg?q=70"
+        href="https://www.flipkart.com/abh-lifestyle-men-kurta-waistcoat-pyjama-set/p/itm5bdd878478bf0?pid=ETHFSHY4VMYVFWGZ&lid=LSTETHFSHY4VMYVFWGZXB8EEQ&marketplace=FLIPKART&store=clo%2Fcfv%2Fitg%2Fpme&srno=b_1_35&otracker=browse&fm=search-autosuggest&iid=38e19c98-0559-4f66-8b1b-5337c9ced4fa.ETHFSHY4VMYVFWGZ.SEARCH&ppt=browse&ppn=browse&ssid=iqhppkcuzk0000001668368783721"
+        prag = "Men Kurta, Waistcoat and Pyjama"
+        />
+        <Cards title="Diwan Saheb" 
+        src="https://rukminim1.flixcart.com/image/832/832/kfmv9u80/ethnic-set/m/6/d/42-kdhl-kurta-dhotipant-abh-lifestyle-original-imafwfg8vreyayxn.jpeg?q=70"
+        href="https://www.flipkart.com/abh-lifestyle-men-kurta-dhoti-pant-set/p/itm1469a23876423?pid=ETHFS6WFZYYGXCJS&lid=LSTETHFS6WFZYYGXCJSE1SNAH&marketplace=FLIPKART&fm=productRecommendation%2Fsimilar&iid=R%3As%3Bp%3AETHFMBYUFDBMT5UD%3Bl%3ALSTETHFMBYUFDBMT5UD0QPCJP%3Bpt%3App%3Buid%3A9a6c38a5-638f-11ed-ad3f-c503fa01853a%3B.ETHFS6WFZYYGXCJS&ppt=pp&ppn=pp&ssid=iqhppkcuzk0000001668368783721&otracker=pp_reco_Similar%2BProducts_9_ABH%2BLifestyle%2BMen%2BKurta%2Band%2BDhoti%2BPant%2BSet_ETHFS6WFZYYGXCJS_productRecommendation%2Fsimilar_8&otracker1=pp_reco_NA_productRecommendation%2Fsimilar_Similar%2BProducts_DESKTOP_HORIZONTAL_productCard_cc_9_NA_view-all&cid=ETHFS6WFZYYGXCJS"
+        prag = "Solid Kurta"
         />
       </div>
     </> 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,8 +5,8 @@ function Cards(props){
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.prag}</p>
-                <a href="/" className="btn btn-primary">
-                    Go somewhere
+                <a href={props.href} className="btn btn-primary">
+                    BUY NOW
                 </a>
             </div>
         </div>
@@ -14,3 +14,4 @@ function Cards(props){
 }
 
 export default Cards;
+
